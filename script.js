5749c63ec16780168e2795cab331d21ae8d361b5
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-06-18 14:01:57 +0200

    fixed the input error and added a variable for the error message

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner form input");
+const msg = document.querySelector(".top-banner .msg");
+
+const apiKey = "14d674dd08475e037565e3a068a05b95";
+
 form.addEventListener("submit", e => {
   e.preventDefault();
+
   const inputVal = input.value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // do stuff with the data
+      console.log(data);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city 😩";
+    });
 });
-
-const input = document.querySelector(".top-banner form input")
-const inputVal = input.value
-const apiKey = "14d674dd08475e037565e3a068a05b95"
-
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
-fetch(url)
