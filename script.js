77a3940e139ee6cc7bacabcdbed9925f8230c3e1
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-06-18 13:49:16 +0200

    fixed the undefinded input

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ form.addEventListener("submit", e => {
   const inputVal = input.value;
 });
 
+const input = document.querySelector(".top-banner form input")
 const inputVal = input.value
 const apiKey = "14d674dd08475e037565e3a068a05b95"
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+fetch(url)
