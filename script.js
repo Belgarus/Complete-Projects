da51fcc52825c48f7477c8bd8c096dc2f8ca28f6
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-06-18 14:05:58 +0200

    added the Cards (wheather) and a reset from the Input field (copy and paste)

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ form.addEventListener("submit", e => {
       msg.textContent = "Please search for a valid city 😩";
     });
 });
+
+const { main, name, sys, weather } = data;
+const icon = `https://openweathermap.org/img/wn/${
+  weather[0]["icon"]
+}@2x.png`;
+const li = document.createElement("li");
+li.classList.add("city");
+const markup = ` 
+<h2 class="city-name" data-name="${name},${sys.country}"> 
+<span>${name}</span> 
+<sup>${sys.country}</sup> 
+</h2> 
+<div class="city-temp">${Math.round(main.temp)}<sup>°C</sup> 
+</div> 
+<figure> 
+<img class="city-icon" src=${icon} alt=${weather[0]["main"]}> 
+<figcaption>${weather[0]["description"]}</figcaption> 
+</figure> 
+`;
+li.innerHTML = markup;
+list.appendChild(li);
+
+msg.textContent = "";
+form.reset();
+input.focus();
