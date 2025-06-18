114685d241fbfc91bc67846ee36f78002112beb1
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-06-18 13:34:02 +0200

    created a inputVal and API-Key variable

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ form.addEventListener("submit", e => {
   e.preventDefault();
   const inputVal = input.value;
 });
+
+const inputVal = input.value
+const apiKey = "14d674dd08475e037565e3a068a05b95"
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
