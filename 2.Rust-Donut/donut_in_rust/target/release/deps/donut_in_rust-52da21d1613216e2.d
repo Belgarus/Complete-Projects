8e98b262aa784bd39e3d69ac8ae2bc0cb8388ef1
Belgarus <Club.Mate@tutamail.com>
2025-12-09 15:01:52 +0100

    final build of the rust project

---
+++ 2.Rust-Donut/donut_in_rust/target/release/deps/donut_in_rust-52da21d1613216e2.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/10145000/Documents/Complete-Projects/2.Rust-Donut/donut_in_rust/target/release/deps/donut_in_rust-52da21d1613216e2.d: src/main.rs
+
+/mnt/c/Users/10145000/Documents/Complete-Projects/2.Rust-Donut/donut_in_rust/target/release/deps/donut_in_rust-52da21d1613216e2: src/main.rs
+
+src/main.rs:
