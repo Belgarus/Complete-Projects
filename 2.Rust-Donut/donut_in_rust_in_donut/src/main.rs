d725d94965ee55b36edc4c18dda0107afe6ff728
Belgarus <Club.Mate@tutamail.com>
2025-12-10 14:09:17 +0100

    fixed a commment mistake

--- 2.Rust-Donut/donut_in_rust_in_donut/src/main.rs
+++ 2.Rust-Donut/donut_in_rust_in_donut/src/main.rs
@@ -1,13 +1,12 @@
-                    //~::==!!!**--***!!!+
              use std::io::{stdout,Write};//|\\
           fn main(){let(mut a,mut b)=(0.0f32,0.0f32);
        let(mut z,mut s)=([0.;1760],[b' ';1760]);print!("  
     \x1b[2J");loop{s.fill(b' ');z.fill(0.);for u in 0..90{ 
-  let j=u as f32*0.07;               for w in 0..314{let i=w as
- f32*0.02;let(c,d,e,                   f,g,l,m,n)=(i.sin(),j.cos
- (),a.sin(),j.sin(),                   a.cos(),i.cos(),b.cos(),b
- .sin());let h=d+2.;                   let p=1.0/(c*h*e+f*g +5.0
-  );let t =c*h*g-f*e;                 let x=(40.+30.*p *(l*h*m-
+  let j=u as f32*0.07;for             w in 0..314{let i=w as
+ f32*0.02;let(c,d,e,f,                   g,l,m,n)=(i.sin(),j.cos
+ (),a.sin(),j.sin(),a.                   cos(),i.cos(),b.cos(),b
+ .sin());let h=d+2.;let                   p=1.0/(c*h*e+f*g +5.0
+  );let t =c*h*g-f*e;let                 x=(40.+30.*p *(l*h*m-
    t*n))as i32;let y=(12.            +15.*p*(l*h*n+t*m))as i32;
     let o=(x+80*y)as usize;let q=(8.*((f*e-c*d*g)*m-c*d*e-f*g
     -l*d*n))as i32;if 0<y&&y<22&&0<x&&x<80&&p>z[o]{z[o]=p;s[
@@ -15,4 +14,4 @@
       "\x1b[H");for k in 0..1760{print!("{}",if k%80<1
          {'\n'}else{s[k]as char});}stdout().flush
              ().unwrap();a+=0.01; b+=0.005;}}
-                 /*~~+-.,-------,\\\\*////
+                 //~~+-.,-------,\\\\////
