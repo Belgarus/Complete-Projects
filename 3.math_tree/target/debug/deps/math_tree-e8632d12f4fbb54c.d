0497386c64f6818c3550404d22ccc16e0339469a
Belgarus <Club.Mate@tutamail.com>
2025-12-18 09:02:09 +0100

    added a few new topics

---
+++ 3.math_tree/target/debug/deps/math_tree-e8632d12f4fbb54c.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/10145000/Documents/Complete-Projects/3.math_tree/target/debug/deps/math_tree-e8632d12f4fbb54c.d: src/main.rs
+
+/mnt/c/Users/10145000/Documents/Complete-Projects/3.math_tree/target/debug/deps/math_tree-e8632d12f4fbb54c: src/main.rs
+
+src/main.rs:
---
+++ 3.math_tree/target/debug/math_tree.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/10145000/Documents/Complete-Projects/3.math_tree/target/debug/math_tree: /mnt/c/Users/10145000/Documents/Complete-Projects/3.math_tree/src/main.rs
