2afdd330777ec917ccb0975eca94664cb1442e98
Noah <179312058+Belgarus@users.noreply.github.com>
2026-01-23 20:18:16 +0100

    Delete 2.rust_donut/donut_in_rust/target directory

--- 2.rust_donut/donut_in_rust/target/release/donut_in_rust.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/10145000/Documents/Complete-Projects/2.Rust-Donut/donut_in_rust/target/release/donut_in_rust: /mnt/c/Users/10145000/Documents/Complete-Projects/2.Rust-Donut/donut_in_rust/src/main.rs
